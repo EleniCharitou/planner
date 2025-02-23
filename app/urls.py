ace509a5e88df6cea9dd98ba7d882229ccbd1539
eleni <elenixar7879@gmail.com>
2025-02-23 09:21:19 +0100

    Correct urls

--- app/urls.py
+++ app/urls.py
@@ -1,17 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
 from rest_framework import routers
 
-# router = routers.DefaultRouter()
+from .views import TripViewSet
 
-# router.register(r'trips', views.TripViewset, basename='trips')
+router = DefaultRouter()
+router.register(r'trip', TripViewSet, basename='trip')
+
 
 urlpatterns = [
-	path('', views.AppOverview, name='home'),
+    path('', include(router.urls)),
+	path('', views.app_overview, name='home'),
     path('create/', views.add_items, name='add-items'),
     path('all/', views.view_items, name='view-items'),
     path('update/<int:pk>', views.update_items, name='update-items'),
     path('item/<int:pk>/delete/', views.delete_items, name='delete-items'),
- 
-    # path('trips/', views.TripViewset, name='trips'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,75 +6,71 @@ from .serializers import TripSerializer, ItemSerializer
 from rest_framework import serializers, status, viewsets
 
 
-class TripViewset(viewsets.ModelViewSet):
-	queryset = Trip.objects.all()
-	serializer_class = TripSerializer
-	
-    # def get_queryset(self):
-	# 	return Trip.objects.filter(user=self.request.user)
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
 
 
+# Items views
 @api_view(['GET'])
-def AppOverview(request):
-	api_urls = {
-		'all_items': '/',
-		'Search by Category': '/?category=category_name',
-		'Search by Subcategory': '/?subcategory=category_name',
-		'Add': '/create',
-		'Update': '/update/pk',
-		'Delete': '/item/pk/delete'
-	}
+def app_overview(request):
+    api_urls = {
+        'all_items': '/',
+        'Search by Category': '/?category=category_name',
+        'Search by Subcategory': '/?subcategory=category_name',
+        'Add': '/create',
+        'Update': '/update/pk',
+        'Delete': '/item/pk/delete'
+    }
 
-	return Response(api_urls)
+    return Response(api_urls)
 
 
 @api_view(['POST'])
 def add_items(request):
     item = ItemSerializer(data=request.data)
- 
+
     # validating for already existing data
     if Item.objects.filter(**request.data).exists():
         raise serializers.ValidationError('This data already exists')
- 
+
     if item.is_valid():
         item.save()
         return Response(item.data)
     else:
         return Response(status=status.HTTP_404_NOT_FOUND)
-    
+
 
 @api_view(['GET'])
 def view_items(request):
-	
-	# checking for the parameters from the URL
-	if request.query_params:
-		items = Item.objects.filter(**request.query_params.dict())
-	else:
-		items = Item.objects.all()
+    # checking for the parameters from the URL
+    if request.query_params:
+        items = Item.objects.filter(**request.query_params.dict())
+    else:
+        items = Item.objects.all()
 
-	# if there is something in items else raise error
-	if items:
-		serializer = ItemSerializer(items, many=True)
-		return Response(serializer.data)
-	else:
-		return Response(status=status.HTTP_404_NOT_FOUND)
-	
+    # if there is something in items else raise error
+    if items:
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
 
 @api_view(['POST'])
 def update_items(request, pk):
-	item = Item.objects.get(pk=pk)
-	data = ItemSerializer(instance=item, data=request.data)
+    item = Item.objects.get(pk=pk)
+    data = ItemSerializer(instance=item, data=request.data)
 
-	if data.is_valid():
-		data.save()
-		return Response(data.data)
-	else:
-		return Response(status=status.HTTP_404_NOT_FOUND)
+    if data.is_valid():
+        data.save()
+        return Response(data.data)
+    else:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['DELETE'])
 def delete_items(request, pk):
-	item = get_object_or_404(Item, pk=pk)
-	item.delete()
-	return Response(status=status.HTTP_202_ACCEPTED)
-
+    item = get_object_or_404(Item, pk=pk)
+    item.delete()
+    return Response(status=status.HTTP_202_ACCEPTED)
