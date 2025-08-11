a8a34a274265c4b52ad67507002c930021a3fde7
eleni <elenixar7879@gmail.com>
2025-08-11 20:34:56 +0200

    Setting fixes

--- planner/settings.py
+++ planner/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 if not DEBUG:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
