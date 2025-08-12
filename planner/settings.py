c507c3e865c3840c96f75ee37aa5f2a427894a33
eleni <elenixar7879@gmail.com>
2025-08-12 21:34:02 +0200

    Add codesandbox's cors allowed origin

--- planner/settings.py
+++ planner/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS", "").split(",") if os.environ.get("CORS_ALLOWED_ORIGINS") else [
+    "https://sr6q75-5173.csb.app",
     "http://localhost:5173",
     "http://127.0.0.1:5174"
 ]
